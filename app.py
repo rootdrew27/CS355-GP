326d94a8c6cbff799758ac1c2b29adaa1dbb4461
Andrew Root <root.drew27@gmail.com>
2024-03-25 15:54:02 -0500

    new views. added index

--- app.py
+++ app.py
@@ -1,25 +1,46 @@
 """test Flask with this"""
 
-from flask import Flask, render_template, request, url_for, session
+from flask import Flask, render_template, request, url_for, session, redirect
+from flask_session import Session
 import sqlite3
 from markupsafe import escape
+from datetime import timedelta
+import json
+
 
 app = Flask(__name__)
 
-app.secret_key = b'JF*FIWazxa3'
-
+# app.config.from_file('./config.json', load=json.load)
+# Session(app)
+# app.config['SESSION_TYPE'] = 'memcached' # session info is stored via memcache api
+# app.config['SESSION_PERMANENT'] = True # session are persisent, thus they are not ended when the brwoser closes
+# app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60) # sessions lasts for 60 minutes unless renewed or explicitly cancelled.
+# app.config['SESSION_REFRESH_EACH_REQUEST'] = True # session renew feature is enabled
+#app.secret_key = b'JF*FIWazxa3' 
 
 ######################## VIEWS #####################################
 
 # home view
 @app.route('/')
 def home():
-    return render_template("test.html", endpoint='test')
+    endpoint = url_for('job_finder')
+    return render_template('index.html', endpoint=endpoint)
 
-# test view
-@app.route('/test')
-def test():
-    return f'success'
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login(prev_page: str):
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for(prev_page))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
 
 # job list view
 @app.get('/jobs')
@@ -30,6 +51,9 @@ def job_finder():
     conn.close()
     return render_template('jobs.html', jobs=jobs)
 
+@app.get('/jobs/<int:job_id>')
+def job_page():
+    pass
 
 
 ################## HELPER FUNCTIONS #################################
@@ -43,6 +67,7 @@ def get_db_conn():
     conn.row_factory = sqlite3.Row
     return conn
 
+perm_lvls = {0: 'default', 1: 'student', 2: 'faculty'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,6 @@ CREATE TABLE jobs (
     date_listed TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     job_description TEXT NOT NULL
 )
+
+-- additionally, jobs needs a foreign key to User, to allow a poster to be attached to the job
+-- and a fk for a department
