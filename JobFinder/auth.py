ca4d7ddef05ad26d9b4cbe85915cdd69df182984
Rain <rtryba@gmail.com>
2024-05-01 16:54:51 -0500

    Fotter at bottom, resume upload partial

--- JobFinder/auth.py
+++ JobFinder/auth.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for, session, redirect, flash
-from flask_session import Session
+#from flask_session import Session
 from flask import Blueprint
 from JobFinder.helpers import get_db_conn, is_logged_in, send_dfa_token
 
