3771d66b5b096b196f487eed359b869b9c209055
McKenzie <mckenziemmack@gmail.com>
2024-05-04 09:53:04 -0500

    department

--- JobFinder/db/init_db.py
+++ JobFinder/db/init_db.py
@@ -11,7 +11,7 @@ with open('./db/schema.sql') as f:
 cur = connection.cursor()
 cur.execute(f"INSERT INTO user (first_n, last_n, email, password, permission_level) VALUES ('Steve', 'Thestudent', 'Steve@gmail.com', 'password', 1)")
 cur.execute(f"INSERT INTO user (first_n, last_n, email, password, permission_level) VALUES ('Suzy', 'Theteacher', 'Suzy@gmail.com', 'password', 2)")
-cur.execute(f"INSERT INTO department (title, descrip) VALUES ('Computer Science', 'A department for computer stuff')")
+cur.execute(f"INSERT INTO department (title, descrip, website_url, email) VALUES ('Learning & Technology Services', 'LTS provides the campus community with instructional and informational technology tools as well as a robust networking infrastructure and the support to foster all aspects of student learning and academic excellence.', 'https://www.uwec.edu/lts', 'helpdesk@uwec.edu')")
 cur.execute(f"INSERT INTO job (title, descrip, img_path, user_id, dept_id) VALUES ('Programmer', 'Write Programs', 'c', 2, 1)")
 cur.execute(f"INSERT INTO job (title, descrip, img_path, user_id) VALUES ('Janitor', 'Perform a variety of cleaning tasks.', '../static/images/janitor.jpg', 2)")
 cur.execute(f"INSERT INTO user (first_n, last_n, email, password, permission_level) VALUES ('Sally', 'Theotherteacher', 'Sally@gmail.com', 'password', 2)")
